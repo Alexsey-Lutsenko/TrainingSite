ac3f96ef86dba2ef5886b878091243afc761ca89
Alexsey Lutsenko <alexsey.lutsenko@yandex.ru>
2020-12-04 18:26:37 +0700

    learn_day_1

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://alexsey_lutsenko:Fktrctq12129495.@cluster0.fbul2.mongodb.net/TrainingSite?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
+}
---
+++ controllers/auth.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const keys = require('../config/keys')
+
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        // Password check, User found
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            // Generation Token
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60})
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            // Password not math
+            res.status(401).json({
+                message: 'Пароли не совпадают. Попробуйте снова.'
+            })
+        }
+    } else {
+        // User not found
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден.'
+        })
+    }
+}
+
+module.exports.register = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        // User already created
+        res.status(409).json({
+            massage: 'Email уже занят, попробуйте другой.'
+        })
+    } else {
+        // new User create
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+            // Error handler
+        }
+    }
+
+}
