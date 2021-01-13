582e2ce81da073a838facb5d900247c0e1e2809e
Alexsey Lutsenko <alexsey.lutsenko@yandex.ru>
2021-01-13 17:56:54 +0700

    learn_day_6

---
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics',
+  templateUrl: './analytics-page.component.html',
+  styleUrls: ['./analytics-page.component.css']
+})
+export class AnalyticsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/order-page/order-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order-page.component.html',
+  styleUrls: ['./order-page.component.css']
+})
+export class OrderPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
